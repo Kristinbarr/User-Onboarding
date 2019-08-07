555ea93bfbde0faf0c34e9e71009c5144ceb9387
Kristin Barr <kristinbarr@gmail.com>
2019-08-07 18:11:57 -0400

    initial commit and basic Form component

---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Form extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>test</div>
+    )
+  }
+}
+
+export default Form
